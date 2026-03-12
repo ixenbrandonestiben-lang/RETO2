afbbb135375531be87d093e2fdcbde199e4f3e4f
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-03-12 08:52:58 -0600

    feat: :sparkles: agregamos el ciclo while
    nos ayudara aejecutar y mandar allamar anuestra funcion para seguir con el resto del procedimiento

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def calcular():
     imc = peso / (altura ** 2)
     print("su indice de masa corporal es: ", imc)
 
+
+while True:
+    print("bienvenido al calculador de indice de masa corporal")
+    print("1. calcular indice de masa corporal")
+    print("2. salir")
+    opcion = input("ingrese su opcion: ")
+    if opcion == "1":
+        calcular()
+    elif opcion == "2":
+        print("gracias por usar el calculador de indice de masa corporal")
+        break
+    else:
+        print("opcion no valida, por favor intente de nuevo")
+        
